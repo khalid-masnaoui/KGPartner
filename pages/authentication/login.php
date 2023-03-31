0bbc3a0d87be67fa0a8c176712108b86724d5d54
khalid-masnaoui <elmasnaouikhalid1@gmail.com>
2023-03-31 16:39:36 +0000

    we

--- pages/authentication/login.php
+++ pages/authentication/login.php
@@ -101,7 +101,7 @@ function insertLoginAttempt($db, $array, $status, $detail)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Korea Gaming LogIn</title>
     <link rel="icon" type="image/x-icon" href="/assets/images/favicon.png?v=1.00" />
-    <link rel="stylesheet" href="/assets/css/login.css?v=1.03">
+    <link rel="stylesheet" href="/assets/css/login.css?v=1.04">
 
     <style>
     @media (max-width: 410px) {
