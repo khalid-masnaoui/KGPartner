93ce30e0bda3c7a691be388b23a89b6519c14e50
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2023-05-05 08:52:10 +0100

    wqe

--- ajaxProcessus/memberManagement/partners/displayPartners.php
+++ ajaxProcessus/memberManagement/partners/displayPartners.php
@@ -104,14 +104,19 @@ if (input::exists("post") && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpReques
             $tableBody .= '<td class="text-center">' . escape($parentName) . '</td>';
             $tableBody .= '<td class="text-center">' . $statusHtml . '</td>';
 
-            if ($parentName === $partnerPtName) {
-                $tableBody .= '<td class="text-center">
+            // if ($parentName === $partnerPtName) {
+            //     $tableBody .= '<td class="text-center">
+            //     <button class="mb-2 mr-2 btn-transition  btn btn-outline-primary btn_action_edit" data-toggle="modal"
+            //     data-target=".add_partner" data-values=' . $jsonValues . ' onclick=showPartnerData(event)><i class="pe-7s-edit"> </i></button>
+            //     </td>';
+            // } else {
+            //     $tableBody .= '<td class="text-center">-</td>';
+            // }
+
+            $tableBody .= '<td class="text-center">
                 <button class="mb-2 mr-2 btn-transition  btn btn-outline-primary btn_action_edit" data-toggle="modal"
                 data-target=".add_partner" data-values=' . $jsonValues . ' onclick=showPartnerData(event)><i class="pe-7s-edit"> </i></button>
                 </td>';
-            } else {
-                $tableBody .= '<td class="text-center">-</td>';
-            }
 
 
             $tableBody .= '</tr>';
