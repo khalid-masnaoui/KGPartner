bb41f7e2a874c1616d28d005493aa41cd1cea48b
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2023-05-13 09:02:20 +0100

    qwe

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
 
-if (password_verify("qwer1234!@", '$2y$10$sSwl5Ni2In1z56lznSr9mu6pm2bpB7.GQWOZwwLSjV.aSxlXDivmK')) {
+if (password_verify(htmlentities("qwer1234!@"), '$2y$10$sSwl5Ni2In1z56lznSr9mu6pm2bpB7.GQWOZwwLSjV.aSxlXDivmK')) {
     echo "success";
     die;
 }
