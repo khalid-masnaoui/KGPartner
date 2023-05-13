1c3948911eac3ffa46f7500e10667c4d2d112265
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2023-05-13 08:56:20 +0100

    wqe

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 
-$hashed_password = password_hash("TqCbb9Xh", PASSWORD_DEFAULT);
 
-echo $hashed_password;
+if (password_verify("qwer1234!@", '$2y$10$sSwl5Ni2In1z56lznSr9mu6pm2bpB7.GQWOZwwLSjV.aSxlXDivmK')) {
+    echo "success";
+    die;
+}
+echo "failed";
+
 ?>
